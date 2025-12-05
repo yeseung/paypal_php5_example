b4f5151461b1fc7c11cff173b8f405e2bd460a3c
_____________seung <hayeseung@gmail.com>
2025-12-05 10:05:45 +0900

    Update cancel.php

--- cancel.php
+++ cancel.php
@@ -1,1 +1,12 @@
-<h1>결제 취소</h1>
+<!-- cancel.php -->
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="utf-8">
+  <title>결제 취소</title>
+</head>
+<body>
+  <h1>결제가 취소되었습니다.</h1>
+  <p>다시 시도하시려면 결제 페이지로 돌아가 주세요.</p>
+</body>
+</html>
