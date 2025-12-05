860145a154ee9009b3b326cc2eb06a596b01ac13
_____________seung <hayeseung@gmail.com>
2025-12-05 10:06:05 +0900

    Update success.php

--- success.php
+++ success.php
@@ -1,1 +1,15 @@
-<h1>결제 완료</h1>
+<!-- success.php -->
+<?php
+// 여기는 단순히 "결제가 완료되었습니다" 라고 안내하는 용도
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="utf-8">
+  <title>결제 완료</title>
+</head>
+<body>
+  <h1>결제가 완료되었습니다.</h1>
+  <p>감사합니다. 이제 광고 제거 기능이 활성화됩니다.</p>
+</body>
+</html>
